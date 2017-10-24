a197d9142b57ebd2dcbda31f162b853a4fb7de47
Jack <jsjutzi@gmail.com>
2017-10-24 14:59:08 -0500

    Fully Functional

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import {BrowserRouter} from 'react-router-dom';
+
 
 import "./index.css";
 
@@ -9,8 +11,10 @@ import store from "./store";
 import App from "./components/App";
 
 ReactDOM.render(
-	<Provider store={ store }>
-		<App />
-	</Provider>,
+	<BrowserRouter>
+		<Provider store={ store }>
+			<App />
+		</Provider>
+	</BrowserRouter>,
 	document.getElementById( "root" )
 );
